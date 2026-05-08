d38f7c405bfbbbf98ba1f149c5ae56b59767cd81
Pilar-cmd <giihrapozinha13@gmail.com>
2026-05-08 15:51:44 -0300

    thor

--- Assets/AddPomits.cs
+++ Assets/AddPomits.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class AddPoints : MonoBehaviour
 {
+    public AudioSource source;
     public LixoSpawnerController lixoSpawnerController;
 
     private void OnCollisionEnter(Collision collision) {
         // Optional: Check if the entering object is the Player
         if (collision.gameObject.CompareTag("Lixo")) {
             Destroy(collision.gameObject);
+            source.Play();
             lixoSpawnerController.AddToPoints(1);
         }
     }
